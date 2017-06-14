2d5f6463f0a8cbf3e591c9bd9866d8ee931934bb
9one9 <brayson@albany.edu>
2017-06-14 13:38:42 -0400

    Update functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
- function button1() {
-    document.getElementById("1").innerHTML = "1";
+/*jslint white:true */
+/*global angular */
+
+var app = angular.module("calcApp", []);
+app.controller("CalcCtrl", CalcCtrl);
+
+
+function CalcCtrl() {
+    this.gotClicked = function(button) {
+        this.selectOperator = button;
+    }
+
+     //computing   
+this.compute = function() {
+    var num1 = parseFloat(this.input1);
+    var num2 = parseFloat(this.input2);
+        if (this.selectOperator === "+"){
+            this.result = num1 + num2;
         }
+        else if (this.selectOperator === "-"){
+            this.result = num1 - num2;
+        }
+        else if (this.selectOperator === "/"){
+            this.result = num1 / num2;
+        }
+        else if (this.selectOperator === "*"){
+            this.result = num1 * num2;
+        }
+}
+
+
+}
+
+/*
+function theFunc() {
+    var x = document.getElementsByTagName("BUTTON")[0].textContent;
+    document.getElementById("numLine").innerHTML = x;  
+}
+
+*/
